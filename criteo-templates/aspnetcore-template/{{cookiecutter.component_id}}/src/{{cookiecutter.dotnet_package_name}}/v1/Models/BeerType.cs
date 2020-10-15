6a86c9eec8c09c5938d54e1e0ee1ad01aba1127f
canut <michelevski@gmail.com>
2020-10-15 16:18:48 +0200

    Create aspnetcore template

---
+++ criteo-templates/aspnetcore-template/{{cookiecutter.component_id}}/src/{{cookiecutter.dotnet_package_name}}/v1/Models/BeerType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Criteo.Api.Exam.Demo.v1.Models
+{
+    public enum BeerType
+    {
+        Blond,
+        Brown,
+        Amber,
+    }
+}
